4e822f1e6c854d312a1e581903e4f4dbc936e87f
yuyongkai <yuyongkai@vread.cn>
2018-04-23 10:14:11 +0800

    add utils

--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,7 @@
+const {
+  baseUrl,
+} = require('../config/index.js');
+
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -14,6 +18,65 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const getPaths = (url = '') => {
+  if (!/^(http[s]?:)/.test(url) ) {
+    return baseUrl + url;
+  }
+  return url;
+}
+const ajax = (url, cfg = { data:{}}) => {
+
+  const {
+    data,
+    header,
+    method,
+    dataType,
+    complete,
+    responseType,
+  } = cfg;
+
+  const promise = new Promise((resolve, reject) => {
+    
+    wx.request({
+      url: getPaths(url),
+      data,
+      header,
+      method,
+      dataType,
+      responseType,
+      success({ data, statusCode, header }) {
+        resolve(data, statusCode, header)
+      },
+      fail({ data, statusCode, header }) {
+        resolve(data, statusCode, header)
+      },
+      complete,
+    });
+  })
+
+  return promise;
+
+}
+
+const post = (url, cfg = {method: 'POST'}) => {
+  return ajax(url, cfg)
+}
+
+const get = (url, cfg = {method: 'GET' }) => {
+  return ajax(url, cfg);
+}
+
+const request = (url, cfg = {method: 'GET'}) => {
+  return wx.request({
+    url: getPaths(url),
+    ...cfg,
+  });
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime,
+  ajax,
+  get, 
+  post,
+  request,
 }
