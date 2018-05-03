57fc5bdcc6b64eba896c219bab840f05ddd44c8c
zhilichaoA <649281575@qq.com>
2018-05-03 14:12:21 +0800

    fix

--- components/commentList/commentList.js
+++ components/commentList/commentList.js
@@ -15,6 +15,14 @@ Component({
         replyContent:{ //二级评论
             type: Array, // 类型（必填），目前接受的类型包括：String, Number, Boolean, Object, Array, null（表示任意类型）
             value: null, // 属性初始值（可选），如果未指定则会根据类型选择一个
+        },
+        isReplyContent:{
+            type: Boolean, // 类型（必填），目前接受的类型包括：String, Number, Boolean, Object, Array, null（表示任意类型）
+            value: true, // 属性初始值（可选），如果未指定则会根据类型选择一个
+        },
+        isBorder:{
+            type: Boolean, // 类型（必填），目前接受的类型包括：String, Number, Boolean, Object, Array, null（表示任意类型）
+            value: true, // 属性初始值（可选），如果未指定则会根据类型选择一个
         }
     },
     data: {
@@ -34,6 +42,13 @@ Component({
         //组件生命周期函数，在组件实例被从页面节点树移除时执行
     },
     methods: {
-
+        //点击更多回复按钮 事件
+        clickGoReply(event){
+            let data=JSON.stringify(event.currentTarget.dataset);
+            wx.navigateTo({
+                url:`/pages/reply/reply?data=${data}`
+            })
+            //console.log(event)
+        }
     }
 })
