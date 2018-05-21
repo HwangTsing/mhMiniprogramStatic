081f91854d6dc8be1e701788979d561c5886eaae
yuyongkai <yuyongkai@vread.cn>
2018-05-21 14:37:25 +0800

    modify

--- pages/read/read.js
+++ pages/read/read.js
@@ -165,7 +165,7 @@ Page({
     wxApi.pageScrollTo({ scrollTop })
   },
 
-  render: function (chapter_id = 260743){
+  render: function (chapter_id){
     //wxApi.pageScrollTo({scrollTop: 0});
     if (!chapter_id) return this.setPageMessage('noExist')
     this.fetchComic(chapter_id).then(({ chapter_list, comic_id, chapter_id, chapter_name} = {})=>{
