16f69cfcbc4f41bfc901165cc6a689d8761e195e
yyk310 <yyk310@163.com>
2018-07-23 16:45:54 +0800

    add isDev 开发接口

--- config/index.js
+++ config/index.js
@@ -1,7 +1,9 @@
-const baseUrl = 'https://apiv2.manhua.weibo.com/'
+const isDev = true
+const baseUrl = isDev ? 'http://manhua.weibo.cn/' : 'https://apiv2.manhua.weibo.com/'
 // 测试接口
 //const baseUrl = 'http://manhua.weibo.cn/'
 
 module.exports = {
-  baseUrl
+  baseUrl,
+  isDev
 }
